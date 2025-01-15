1c7ac140648f44cdfcc06403f3db20b4f933b431
zihad18 <zihadmaruf397@gmail.com>
2025-01-15 22:52:02 +0600

    basic component logics done

--- Task_2/tapaScript_cars/src/components/CarItem.jsx
+++ Task_2/tapaScript_cars/src/components/CarItem.jsx
@@ -1,8 +1,33 @@
-function CarItem() {
+import PropTypes from 'prop-types';
+function CarItem({car}) {
   return (
     <div>
-      CarItem
+      <h1 className="text-lg font-bold">{car.title}</h1>
+      <div>
+      <div>
+      <span className="text-md font-bold">Brand:</span>
+        {car.brand}</div>
+      <div>
+        <span className="text-md font-bold">Year:</span>
+        {car.year}</div>
+      <div>
+      <span className="text-md font-bold">Price:</span>
+        {car.price}</div>
+      <div>
+      <span className="text-md font-bold"> Premium:</span>
+        {car.isPremium ? "Yes" : "No"}</div>
+    </div>
     </div>
   );
 }
 export default CarItem;
+
+CarItem.propTypes = {
+    car: PropTypes.shape({
+        title: PropTypes.string,
+        price: PropTypes.number,
+        year: PropTypes.number,
+        brand: PropTypes.string,
+        isPremium: PropTypes.bool
+    })
+};
--- Task_2/tapaScript_cars/src/components/CarList.jsx
+++ Task_2/tapaScript_cars/src/components/CarList.jsx
@@ -1,7 +1,24 @@
+import CarItem from "./CarItem";
+
+const Cars = [   
+    { id: 1, title: "Luxury Sedan", brand: "Mercedes", year: 2023, price: 80000, isPremium: true },
+    { id: 2, title: "Family SUV", brand: "Toyota", year: 2022, price: 45000, isPremium: false },
+    { id: 3, title: "Sports Car", brand: "Porsche", year: 2023, price: 120000, isPremium: true },
+    { id: 4, title: "Electric Hatchback", brand: "Nissan", year: 2022, price: 35000, isPremium: false },
+    { id: 5, title: "Luxury SUV", brand: "BMW", year: 2023, price: 90000, isPremium: true },
+  ];
 function CarList(){
     return (
         <div>
-            CarList
+           <ul className="list-none">
+            {
+                Cars.map((car)=>(
+                    <li key={car.id}>
+                        <CarItem car={car}/>
+                    </li>
+                ))
+            }
+            </ul>
         </div>
     )
 }
