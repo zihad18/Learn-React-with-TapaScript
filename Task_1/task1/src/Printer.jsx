0eaeb6db2d2117032e0c962b540565521106b1e1
zihad18 <zihadmaruf397@gmail.com>
2025-01-15 13:22:53 +0600

    project is ready

---
+++ Task_1/task1/src/Printer.jsx
@@ -0,0 +1,10 @@
+function Printer() {
+  return( 
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+        <div className="text-xl text-green-500 text-center bg-gray-100 p-4 justify-center">
+            We are here to learn React from tapaScript
+        </div>
+  </div>
+  );
+}
+export default Printer;
