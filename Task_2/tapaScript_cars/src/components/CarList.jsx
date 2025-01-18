fb8f260a0154118f496446daa5ef7768154b034b
zihad18 <zihadmaruf397@gmail.com>
2025-01-18 20:56:50 +0600

    functionality added

--- Task_2/tapaScript_cars/src/components/CarList.jsx
+++ Task_2/tapaScript_cars/src/components/CarList.jsx
@@ -1,18 +1,23 @@
 import CarItem from "./CarItem";
 import PropType from 'prop-types'
 function CarList({cars, checkTerm, searchTerm}) {
+    const rows = []
+    cars.forEach((car) => {
+        if (car.title.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1) {
+            return;
+        }
+        if (checkTerm && !car.isPremium) {
+            return;
+        }
+    rows.push(<CarItem key={car.id} car={car}/>);
+    });
 
     return (
         <div className="py-4">
-           <ul className="list-none grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {
-                cars.map((car)=>(
-                    <li key={car.id} className="">
-                        <CarItem car={car}/>
-                    </li>
-                ))
-            }
-            </ul>
+            <div className="list-none grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {rows}
+            </div>
+          
         </div>
     )
 }
